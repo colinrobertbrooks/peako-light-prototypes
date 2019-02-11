1eec4f7c004d9c23f47792493982a290b9c77666
Colin Cummings <colin_cummings@brown.edu>
2019-02-10 20:41:27 -0500

    Updates

---
+++ precache-manifest.a8f94fabcb4a556b13415fc1182cf197.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ccf08c66d06a2515d67",
+    "url": "/peako-light-prototype/static/js/runtime~main.2ccf08c6.js"
+  },
+  {
+    "revision": "f538baab656fdb9bf2c7",
+    "url": "/peako-light-prototype/static/js/main.f538baab.chunk.js"
+  },
+  {
+    "revision": "f0c2bd464e9ba3b8e90c",
+    "url": "/peako-light-prototype/static/js/1.f0c2bd46.chunk.js"
+  },
+  {
+    "revision": "f538baab656fdb9bf2c7",
+    "url": "/peako-light-prototype/static/css/main.2c6bd533.chunk.css"
+  },
+  {
+    "revision": "69b2360577ce1fc8f38557b7a01d401e",
+    "url": "/peako-light-prototype/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-with-flow/precache-manifest.4c792e7e97e2eed321c4781e9c6f823a.js"
+  "/peako-light-prototype/precache-manifest.a8f94fabcb4a556b13415fc1182cf197.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-with-flow/index.html", {
+workbox.routing.registerNavigationRoute("/peako-light-prototype/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
