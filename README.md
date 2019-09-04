0a4ab53707e85d33df3aec3971fe59890d5ff723
Colin Cummings <colin_cummings@brown.edu>
2019-09-04 15:27:54 -0400

    arduino implementation

--- README.md
+++ README.md
@@ -15,3 +15,7 @@
 ## Boilerplate
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## References
+
+[Learning C for Arduino](https://mrwilde.com/wp-content/uploads/2018/03/Learning-C-for-Arduino.pdf)
---
+++ peako.ino
@@ -0,0 +1,69 @@
+/*
+ * hardware
+ */
+int lights[5] = {2, 3, 4, 5, 6};
+int lightCount = 5;
+
+/*
+ * configs
+ */
+int start[5] = {32, 20, 26, 5, 0};
+int duration[5] = {8, 17, 50, 17, 8};
+int loopEnd = 60;
+int delayMs = 1000;
+
+/*
+ * runtime
+ */
+int loopIdx;
+
+/*
+ * utils
+ */
+int setNextLoopIdx() {
+  int start = 0;
+  int reset = loopEnd - 1;
+
+  if(loopIdx < reset) {
+    loopIdx = loopIdx + 1;
+  } else if(loopIdx == reset) {
+    loopIdx = start;
+  }
+  // Serial.println(loopIdx);
+}
+
+bool getLightState(int lightIdx) {
+  int lightStart = start[lightIdx];
+  int lightDuration = duration[lightIdx];
+  int lightEnd = lightStart + lightDuration;
+  bool lightStaysOnThroughLoopEnd = lightEnd > loopEnd;
+
+  if(
+    lightStaysOnThroughLoopEnd &&
+    ((loopIdx >= 0 && loopIdx < lightEnd - loopEnd) ||
+      loopIdx >= lightStart)
+  ) {
+    return HIGH;
+  } else if (lightStart <= loopIdx && lightEnd > loopIdx) {
+    return HIGH;
+  }
+  return LOW;
+}
+
+/*
+ * main
+ */
+void setup() {
+  // Serial.begin(9600);
+  for(int lightIdx = 0; lightIdx < lightCount; lightIdx++){
+    pinMode(lights[lightIdx], OUTPUT);
+  }
+}
+
+void loop() {
+  for(int lightIdx = 0; lightIdx < lightCount; lightIdx++){
+    digitalWrite(lights[lightIdx], getLightState(lightIdx));
+  }
+  delay(delayMs);
+  setNextLoopIdx();
+}
